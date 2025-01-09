b06ea2dcc8a2bde1633c8be2582a84adf7f6eb34
hakimov-cods <hakimovabdusattor32@gmail.com>
2025-01-09 15:42:23 +0300

    v1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './widgets/navbar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+
+  return (
+    <>
+      <h1>Text</h1>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
