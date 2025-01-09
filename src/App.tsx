7be23a1f2d67578e06329692620fd2e3fc69ae91
hakimov-cods <hakimovabdusattor32@gmail.com>
2025-01-09 16:09:31 +0300

    has been created component main

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
-import Navbar from './widgets/navbar'
+import Main from './widgets/main/main'
+import Navbar from './widgets/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
 
   return (
     <>
-      <h1>Text</h1>
+      <Main/>
       <Navbar/>
+      
     </>
   )
 }
